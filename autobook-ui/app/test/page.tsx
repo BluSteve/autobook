e6a683050da5d69cfe0cce3f5bd2abe98b6993c2
BluSteve <steve.scyr@gmail.com>
2024-03-08 15:25:09 +0800

    page.tsx

---
+++ autobook-ui/app/test/page.tsx
@@ -0,0 +1,11 @@
+export default function Main() {
+	return (
+		<form>
+			<input type="text" placeholder="Query"/>
+			<br/>
+			<input type="text" placeholder="Custom Title"/>
+			<br/>
+			<input type="submit" value="Send to Kindle"/>
+		</form>
+	);
+}
