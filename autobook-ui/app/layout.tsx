40c5cbbfb57de44811aa16a27689d0c48be376d4
BluSteve <steve.scyr@gmail.com>
2024-03-08 17:17:14 +0800

    install mantine

--- autobook-ui/app/layout.tsx
+++ autobook-ui/app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import "./globals.css";
+import {MantineProvider} from "@mantine/core";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-		<body className={inter.className}>{children}</body>
+		<body className={inter.className}>
+		<MantineProvider defaultColorScheme='auto'>
+			{children}
+		</MantineProvider>
+		</body>
 		</html>
 	);
 }
